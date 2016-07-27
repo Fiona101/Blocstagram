61ceec3599fdd5bb5a6015141008c81e90a18fa8
Fiona Alpe <fiona.alpe@gmail.com>
2016-07-27 19:18:22 +0300

    Initial Blocstagram commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Fiona Alpe on 7/27/16.
+//  Copyright © 2016 Fiona Alpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
