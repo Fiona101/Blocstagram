a45ffda1b02331a48862f822d8dd760d49995ddd
Fiona Alpe <fiona.alpe@gmail.com>
2016-09-25 22:16:27 +0300

    Checkpoint 31 completed successfully

--- Blocstagram/DataSource.m
+++ Blocstagram/DataSource.m
@@ -12,13 +12,15 @@
 #import "Comment.h"
 
 
+@interface DataSource () {
+    
+    NSMutableArray *_mediaItems;
 
-@interface DataSource ()
+}
 
 @property (nonatomic, strong) NSArray *mediaItems;
 
 @end
-
 
 
 @implementation DataSource
@@ -123,6 +125,42 @@
     }
     
     return [NSString stringWithString:s];
+}
+
+
+
+#pragma mark - Key/Value Observing
+
+- (NSUInteger) countOfMediaItems {
+    return self.mediaItems.count;
+}
+
+- (id) objectInMediaItemsAtIndex:(NSUInteger)index {
+    return [self.mediaItems objectAtIndex:index];
+}
+
+- (NSArray *) mediaItemsAtIndexes:(NSIndexSet *)indexes {
+    return [self.mediaItems objectsAtIndexes:indexes];
+}
+
+- (void) insertObject:(Media *)object inMediaItemsAtIndex:(NSUInteger)index {
+    [_mediaItems insertObject:object atIndex:index];
+}
+
+- (void) removeObjectFromMediaItemsAtIndex:(NSUInteger)index {
+    [_mediaItems removeObjectAtIndex:index];
+}
+
+- (void) replaceObjectInMediaItemsAtIndex:(NSUInteger)index withObject:(id)object {
+    [_mediaItems replaceObjectAtIndex:index withObject:object];
+}
+
+
+
+
+- (void) deleteMediaItem:(Media *)item {
+    NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+    [mutableArrayWithKVO removeObject:item];
 }
 
 
